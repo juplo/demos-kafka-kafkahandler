087641b2e3ba223e9df3c20bfeedbb3b0348761f
Kai Moritz <kai@juplo.de>
2022-06-06 11:43:26 +0200

    Generated an empty project with spring initializr

---
+++ src/main/java/de/juplo/kafka/KafkahandlerApplication.java
@@ -0,0 +1,13 @@
+package de.juplo.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkahandlerApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(KafkahandlerApplication.class, args);
+	}
+}
---
+++ src/test/java/de/juplo/kafka/KafkahandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.juplo.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkahandlerApplicationTests
+{
+	@Test
+	void contextLoads()
+	{
+	}
+}
